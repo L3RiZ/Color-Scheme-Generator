f97f7b71234de547c26fce51b600a698444fbe94
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-02-22 23:21:03 +0100

    finished

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const colorContainer = document.getElementById('color-container');
+const colorContainer = document.getElementById('colors-container');
+const hexIDs = [];
 
 document.getElementById('get-scheme').addEventListener('submit', (e) => {
         e.preventDefault();
-        
         colorContainer.innerHTML = ''
-
-        fetch(`https://www.thecolorapi.com/scheme?hex=${document.getElementById('get-color').value.slice(1, 7)}&mode=${document.getElementById('modes').value}`)
-            .then(response => response.json())
-            .then(data => {
-                data.colors.map(color => {
-                    colorContainer.innerHTML += `
-                    <div class="generated-color" id="${color.hex.clean}"></div>
-                    <p class="hex-code">${color.hex.value}</p>`
-                    document.getElementById(color.hex.clean).style.backgroundColor = color.hex.clean;
-                })
-            })
+        generateColors()
     })
+
+function generateColors() {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${document.getElementById('get-color').value.slice(1, 7)}&mode=${document.getElementById('modes').value}`)
+            .then(response => response.json())
+            .then(data => {           
+                data.colors.map(color => {
+                colorContainer.innerHTML += `
+                <div class="color-container" id="color-container">
+                    <div class="generated-color" id="${color.hex.clean}"></div>
+                    <p class="hex-code">${color.hex.value}</p>
+                </div>`
+                document.getElementById(color.hex.clean).style.backgroundColor = color.hex.clean;
+                hexIDs.push(color.hex.clean)
+        })
+    })
+}
+
+document.addEventListener('click', (e) => {
+    if(hexIDs.includes(e.target.id)) {
+        navigator.clipboard.writeText("#" + e.target.id);
+    } else if (hexIDs.includes(e.target.innerHTML.slice(1, 7))) {
+        navigator.clipboard.writeText(e.target.innerHTML);
+    }
+})
+
+generateColors();
